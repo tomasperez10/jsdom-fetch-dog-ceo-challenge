305f6fc6c895d251c4d326c8acc49d9391aefd22
Tomas Perez <tomasitopg6@gmail.com>
2021-06-02 04:22:22 +0000

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImg(images) {
+  return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+
+  images.forEach(image => {
+    const img = document.createElement('img');
+    img.innerHTML = image;
+    main.appendChild(img);
+  });
+
+}
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchBreed(breeds) {
+  return fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+
+  breeds.forEach(breed => {
+    const ul = document.createElement('ul');
+    ul.innerHTML = breed.name;
+    main.appendChild(breed);
+  });
+
+
+}
